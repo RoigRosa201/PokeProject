de891664260d0e7cea3a519832f1cb44bf96cf75
roig <roigrosa88@gmail.com>
2021-04-24 18:24:47 -0400

    edited html for jquery

--- poke.js
+++ poke.js
@@ -1,7 +1,2 @@
-//const resultFromFetch = fetch('https://pokeapi.co/api/v2/1');
-const getPokes = () => {
-    return fetch('https://pokeapi.co/api/v2/')
-        .then(res => res.json())
-}
 
 //console.log(resultFromFetch);
