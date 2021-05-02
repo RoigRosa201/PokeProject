65b03fe3fd0006136833d264866eff6cf0c1d885
roig <roigrosa88@gmail.com>
2021-05-02 15:12:31 -0400

    added fetch and getelement

--- poke.js
+++ poke.js
@@ -1,31 +1,40 @@
-   //definign here the base url of the api where we getting the data from
+//definign here the base url of the api where we getting the data from
    let baseURL='https://pokeapi.co/api/v2/pokemon/';
     renderContent(1,30);
  
     
     function renderContent(min_length,max_length) {
+        console.log("rendering");
         let html='';
         for(let i= min_length;i <= max_length; i++) {
         let this_html='';
+         //console.log("called");
         
-        $.getJSON(baseURL+i, function(data) {
-                this_html =' <div class="card"> <div class="card-body"><table style="width:100%">';
-                this_html += '<tr><td>Id: '+data.id+'</td><td>Name: '+data.name+'</td><td>Height: '+data.height+'</td><td></td> </tr>';
-                this_html += '<tr><td>Width: '+data.weight+' </td>  <td>Base Experience: '+data.base_experience+'</td><td>Order: '+data.order+'</td> </tr>';
-                this_html += '</table></div></div>';
+        
+        fetch(baseURL + i).then(response=>{
+            return response.json();
+        }).then(data=>{
+            //console.log("data",data)
+            //data = data.body;
+            this_html =' <div class="card"> <div class="card-body"><table style="width:100%">';
+            this_html += '<tr><td>Id: '+data.id+'</td><td>Name: '+data.name+'</td><td>Height: '+data.height+'</td><td></td> </tr>';
+            this_html += '<tr><td>Width: '+data.weight+' </td>  <td>Base Experience: '+data.base_experience+'</td><td>Order: '+data.order+'</td> </tr>';
+            this_html += '</table></div></div>';
             html+=this_html;
-
         }).then(function(){  
-        $("#mainDiv").html(html);
+            document.getElementById("mainDiv").innerHTML = html;
+        // $("#mainDiv").html(html);
         });
-    }
-    }
+        }
+        }
+    
   
-        $("#btnRegenerate").on('click',function(e) {
-            e.preventDefault();
+        //$("#btnRegenerate").on('click',function(e) {
+        document.querySelector("#btnRegenerate").addEventListener('click',function(event){
+            //console.log("clicked");
             let min= Math.floor(Math.random() * 30+1);
             let max= Math.floor(Math.random() * 100+1);
             $("#mainDiv").html('');
-    renderContent(min,max);
-    });
-    
+            renderContent(min,max);
+            e.preventDefault();
+        });
